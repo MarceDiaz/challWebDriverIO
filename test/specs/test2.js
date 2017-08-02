3da3ad58ebcab36bfb274fde14122f81f005b2d5
MarceDiaz <mdd1970@gmail.com>
2017-08-02 00:25:58 -0300

    Clean code. Changed browser to Chrome, just to test.

--- test/specs/test2.js
+++ test/specs/test2.js
@@ -1,16 +1,12 @@
 var assert = require('assert');
-//var webdriverio = require('webdriverio');
-//var options = { desiredCapabilities: { browserName: 'firefox' } };
-//var client = webdriverio.remote(options);
-
-/*var webdriver = require('selenium-webdriver'),
-    By = require('selenium-webdriver').By,
-    until = require('selenium-webdriver').until;*/
-
 
 var EasyLocator='//h2[@id="Easy-Test-Setup"]';
+
+
 describe('webdriver.io page', function() {
+
     it('Test to access elements', function () {
+
         browser.url('http://webdriver.io');
         var myElem = browser.element('#Easy-Test-Setup');
         myElem.waitForVisible();
