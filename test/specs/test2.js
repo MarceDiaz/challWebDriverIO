06ba7925d654ab274cf6075296f77dfac2e740e6
MarceDiaz <mdd1970@gmail.com>
2017-08-02 11:50:18 -0300

    Add test
    Improvements adding steps in the flow and assertions.

--- test/specs/test2.js
+++ test/specs/test2.js
@@ -5,16 +5,27 @@ var EasyLocator='//h2[@id="Easy-Test-Setup"]';
 
 describe('webdriver.io page', function() {
 
-    it('Test to access elements', function () {
+    it('Test to navigate and check elements', function () {
 
         browser.url('http://webdriver.io');
         var myElem = browser.element('#Easy-Test-Setup');
         myElem.waitForVisible();
           console.log("Checking access to textContent");
+
         var textContent = myElem.getAttribute("textContent");
           console.log("---->>>'"+textContent+"'");
-
+          //Validate text of element
         assert.equal(textContent, 'Easy Test Setup');
 
+        //Click on "Developer Guide", top link
+        var linkDeveloper=browser.element("//li/a[@href='/guide.html']");
+        linkDeveloper.waitForVisible();
+        linkDeveloper.click();
+        //browser.timeouts('implicit', 5000);
+
+         //Validate text of element
+         var titleFirstStep = browser.element('#Taking-the-first-step');
+         titleFirstStep.waitForVisible();
+         assert.equal(titleFirstStep.getAttribute("textContent"),"Taking the first step");
     });
 });
